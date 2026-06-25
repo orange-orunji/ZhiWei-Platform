e2c39ae81bdb2c10d7ff777832fab02f1a0af52f
orange-orunji <3078045959@qq.com>
2026-06-25 17:23:01 +0800

    分布式集群更改

--- hm-dianping/src/main/java/com/hmdp/config/RedisConfig.java
+++ hm-dianping/src/main/java/com/hmdp/config/RedisConfig.java
@@ -1,8 +1,10 @@
 package com.hmdp.config;
 
+import io.lettuce.core.ReadFrom;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,8 +18,19 @@ public class RedisConfig {
     public RedissonClient redissonClient() {
         Config config = new Config();
 //        改为虚拟机配置docker部署到虚拟机上
-        config.useSingleServer().setAddress("redis://192.168.161.128:6379")
+        config.useSingleServer().setAddress("redis://192.168.161.128:7000")
         .setPassword("Ww2301079399@");
         return Redisson.create(config);
     }
+
+    /**
+     * 配置Redis 哨兵客户端
+     * @return
+     */
+    //=========================================================
+//    @Bean
+//    public LettuceClientConfigurationBuilderCustomizer clientConfigurationBuilderCustomizer(){
+//        return clientConfigurationBuilder -> clientConfigurationBuilder.readFrom(ReadFrom.REPLICA_PREFERRED);
+//    }
+//    ==========================================================================================
 }
